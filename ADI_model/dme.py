e5d592262ff290e07ffb1664acbfa7eab86b0b55
zerosecos <81179129+zerosecos@users.noreply.github.com>
2021-10-20 15:45:18 -0700

    Fixed a bad library link
    Fixed a bad library link and added MIT license to dme.py

--- ADI_model/dme.py
+++ ADI_model/dme.py
@@ -1,4 +1,10 @@
 #! /usr/bin/env python
+
+#Copyright  2021 <Michael Paul>
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import re
 import sys
 import math
@@ -8,8 +14,6 @@ import argparse
 from os.path import dirname
 import os.path 
 sys.path.append(dirname(__file__)) #adds this file's director to the path
-sys.path.append(os.path.join(os.environ['MP_SCRIPTS'])) #adds this file's director to the path
-#sys.path.append(os.path.join(os.environ['MP_SCRIPTS'],"powerManagement")) #adds this file's director to the path
 import mpUtil
 
 #expects to be initialized with two (x,y) tuples
